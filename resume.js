ad4b46c5ba4105bcec5f024c314e2d6fef76c4a0
Lakshmanan G <121873781+Lakshmanan6306@users.noreply.github.com>
2023-01-20 15:53:20 +0530

    Resume format in JSON
    Resume format in JSON & JSON iteration using for loop

---
+++ resume.js
@@ -0,0 +1,20 @@
+//Create your own resume in JSON
+
+var resume = {
+    "name":"Lakshmanan G",
+    "Mail-id":"Career.laksh@yahoo.com",
+    "Mobile-Number":"9566360758",
+    "Object-Summary":"Highly motivated sales professional with 6+years of experience in providing expertise in strategic lead generation, sales presentation, effective liaising, enterprise sales strategy, creative marketing, and category supervision across diverse sector. Quota-surpassing sales manager with a consistent record of converting sales prospects into loyal, satisfied clients and building a network of referrals to increase sales. ",
+    "Strength":"•Flexible and levelheaded under pressure, Good at planning and Presentation.  •	My Willingness to learn and take on responsibilities and ability to Absorb and adapt to new technology and environment. •	Communication, interpersonal, learning and organizing Skills. •	Dedicated to work in teams to achieve common goals to contributing for the organizational goals.",
+    "Professional-Experience":[{"Simply Electrifying Pvt ltd":"Industry : Machinery & Equipment’s ,Designation : Resident Engineering Chennai ,Period : 1st May 2021 to Till Now ,Responsibilities: • Present, promote and sell products/services using solid arguments to existing and prospective customers. • Perform cost-benefit and needs analysis of existing /potential customers to meet their needs. • Establish, develop and maintain positive business and customer relationships. • Reach out to customer leads through cold calling.•	Expedite the resolution of customer problems and complaints to maximize satisfaction. •Achieve agreed upon sales targets and outcomes within schedule. • Analyze the territory/ markets potential, track sales and status reports. •Supply management with reports on customer needs, problems, interests, competitive activities, and potential for new products and services."},{"Bharti Airtel Ltd":"Industry: Telecom Industry, Designation	:SIC–Team leader, Period:27th August’2015 to 30Th April’2021,Responsibilities: •Lead and motivate team to maximize their potentials •Analyze and interpret trends to facilitate planning.•Forecast volume of future sales to maximize incentive."}],
+    "TECHNICAL SKILLS":"•Microsoft MS Office (MS Word, MS Excel),• Diploma in Computer Application",
+    "ACADEMIC":"•B.E( Electronics communication Engineering ) Aringnar Anna institute of science & technology",
+    "PERSONAL PROFILE":"Father Name:Ganesan T,Date of Birth:06th Jun 1994,Marital Status:married,Nationality:Indian,Languages Proficiency:English,Tamil,Present Address:No-47,Sengunthar Othavadai street,Sholingur,Ranipet-631102",
+    "decelaration":"I hereby declare that the above said information is true and correct to the best of my knowledge",
+    "date":"today's date",
+    "place":"chennai",
+    "signature":"Lakshmanan G"
+}
+
+
+console.log(resume);
